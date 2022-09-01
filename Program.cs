3e5470b8c66d4a42c1a1a1e06ddfef2dd8dde995
silence2k6 <robert.leithner@gmail.com>
2022-09-01 02:44:18 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SlotMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] fillingSlotMachine = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Random randomNumber = new Random();
+
+            for (int i = 0; i < fillingSlotMachine.Length; i++)
+            {
+               fillingSlotMachine[i] = randomNumber.Next(1,9);
+            }
+          
+            Console.WriteLine(String.Join(" ", fillingSlotMachine[0..3]));
+            Console.WriteLine(String.Join(" ", fillingSlotMachine[3..6]));
+            Console.WriteLine(String.Join(" ", fillingSlotMachine[6..9]));
+        }
+    }
+}
